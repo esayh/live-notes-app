6519e98f146608455c3aad5cbe29cd0547519de1
Esay Hernandez <74336400+esayh@users.noreply.github.com>
2022-05-03 15:55:35 -0400

    added link to single note and fetch more button

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,16 +1,42 @@
 import React from "react";
 import { useQuery } from "@apollo/client";
 
-// import LoadButton from "../components/LoadButton";
-import { GET_ALL_NOTES } from "../gql/query";
 import NoteFeed from "../components/NoteFeed";
+import LoadButton from "../components/LoadButton";
+import { GET_ALL_NOTES } from "../gql/query";
 
 const Home = () => {
-  const { data, loading, error } = useQuery(GET_ALL_NOTES);
+  const handleFetch = () => {
+    fetchMore({
+      variables: {
+        cursor: data.noteFeed.cursor,
+      },
+      updateQuery: (prev, { fetchMoreResult }) => {
+        return {
+          noteFeed: {
+            cursor: fetchMoreResult.noteFeed.cursor,
+            hasNextPage: fetchMoreResult.noteFeed.hasNextPage,
+            // combine the new results and the old
+            notes: [...prev.noteFeed.notes, ...fetchMoreResult.noteFeed.notes],
+            __typename: "noteFeed",
+          },
+        };
+      },
+    });
+  };
+
+  const { data, loading, error, fetchMore } = useQuery(GET_ALL_NOTES);
 
   if (loading) return <p>Loading...</p>;
 
   if (error) return <p>Error!</p>;
-  return <NoteFeed notes={data.noteFeed.notes} />;
+  return (
+    <React.Fragment>
+      <NoteFeed notes={data.noteFeed.notes} />
+      {data.noteFeed.hasNextPage && (
+        <LoadButton onClick={handleFetch}>More notes</LoadButton>
+      )}
+    </React.Fragment>
+  );
 };
 export default Home;
